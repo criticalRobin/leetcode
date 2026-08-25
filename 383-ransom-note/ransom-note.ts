c7226701378bb7e5b89c5229d055f1c85430ce59
Matias Tite <matiastite1@gmail.com>
2026-08-25 01:10:19 -0500

    Time: 16 ms (64.16%) | Memory: 61.4 MB (21.06%) - LeetSync

---
+++ 383-ransom-note/ransom-note.ts
@@ -0,0 +1,21 @@
+function canConstruct(ransomNote: string, magazine: string): boolean {
+    let map: Map<string, number> = new Map();
+
+    for (const char of magazine) {
+        if (!map.get(char)) {
+            map.set(char, 1);
+        } else {
+            map.set(char, map.get(char) + 1);
+        }
+    }
+
+    for (const char of ransomNote) {
+        if (map.get(char) === 0 || !map.get(char)) {
+            return false;
+        } else {
+            map.set(char, map.get(char) - 1);
+        }
+    }
+
+    return true;
+};
