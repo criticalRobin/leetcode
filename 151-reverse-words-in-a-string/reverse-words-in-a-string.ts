496010a855abca4003cf03db45973acd9170b788
Matias Tite <matiastite1@gmail.com>
2026-08-26 00:00:37 -0500

    Time: 111 ms (6.43%) | Memory: 64.6 MB (5.10%) - LeetSync

---
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.ts
@@ -0,0 +1,30 @@
+function reverseWords(s: string): string {
+    let words: string[] = [];
+    let word: string = '';
+
+    for (let i: number = 0; i < s.length; i++) {
+        if (s[i] !== ' ') {
+            console.log(s[i]);
+            word += s[i];
+        }
+
+        if (word !== '' && (s[i] === ' ' || i === s.length - 1)) {
+            words.push(word);
+            // console.log(word.length);
+            word = '';
+        }
+    }
+
+    // console.log(`words = ${words.length}`);
+
+    for (let j: number = words.length - 1; j >= 0; j--) {
+        console.log(`word = ${words[j]}`);
+        if (j > 0) {
+            word += words[j] + ' ';
+        } else {
+            word += words[j];
+        }
+    }
+
+    return word;
+};
