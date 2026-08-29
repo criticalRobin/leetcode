5c2794fb3c69d803c0c051debeed8b1280f927cd
Matias Tite <matiastite1@gmail.com>
2026-08-29 13:42:02 -0500

    Time: 28 ms (16.63%) | Memory: 58.4 MB (56.56%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.ts
@@ -0,0 +1,6 @@
+function isAnagram(s: string, t: string): boolean {
+    const sortedS: string = s.split('').sort().join('');  
+    const sortedT: string = t.split('').sort().join('');
+
+    return sortedS === sortedT;  
+};
