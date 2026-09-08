a6a2208857f70ad9b466ed0e22c5e1829d564fc5
Matias Tite <matiastite1@gmail.com>
2026-09-08 09:22:47 -0500

    Time: 54 ms (16.58%) | Memory: 84.6 MB (20.92%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.ts
@@ -0,0 +1,16 @@
+function containsNearbyDuplicate(nums: number[], k: number): boolean {
+    let hasSeen: Map<number, number> = new Map();
+
+    for (let i: number = 0; i < nums.length; i++) {
+        if (!hasSeen.has(nums[i])) {
+            hasSeen.set(nums[i], i);           
+        } else {
+            if (Math.abs(hasSeen.get(nums[i]) - i) <= k) return true;
+
+            hasSeen.set(nums[i], i);
+        } 
+        
+    }
+
+    return false;
+};
