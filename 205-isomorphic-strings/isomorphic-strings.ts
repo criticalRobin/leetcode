8f8c673d946ac945f2f39cf34bcf73effff82188
Matias Tite <matiastite1@gmail.com>
2026-09-09 15:23:03 -0500

    Time: 4 ms (89.80%) | Memory: 56.7 MB (67.44%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.ts
+++ 205-isomorphic-strings/isomorphic-strings.ts
@@ -3,14 +3,12 @@ function isIsomorphic(s: string, t: string): boolean {
     let tMap: Map<string, string> = new Map();
     
     for (let i: number = 0; i < s.length; i++) {
-        const char: string = s[i];
-        
-        if (!sMap.has(char) && !tMap.has(t[i])) {
-            sMap.set(char, t[i]);
-            tMap.set(t[i], char);
+        if (!sMap.has(s[i]) && !tMap.has(t[i])) {
+            sMap.set(s[i], t[i]);
+            tMap.set(t[i], s[i]);
         }
 
-        if (sMap.get(char)! !== t[i]) return false;
+        if (sMap.get(s[i])! !== t[i]) return false;
     }
 
     return true;
