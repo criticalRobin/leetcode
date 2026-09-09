9ca7fc46dc4a4fa68a9eb67742ff3449819e0ae1
Matias Tite <matiastite1@gmail.com>
2026-09-09 18:39:04 -0500

    Time: 1 ms (81.58%) | Memory: 57.7 MB (41.58%) - LeetSync

--- 202-happy-number/happy-number.ts
+++ 202-happy-number/happy-number.ts
@@ -8,13 +8,10 @@ function isHappy(n: number): boolean {
         for (let digit of String(num)) sum += Number(digit) ** 2;
 
         if (sum === 1) return true;
-        
-        if (!seen.has(sum)) {
-            seen.add(sum);
-        } else {
-            return false;
-        }
 
+        if (seen.has(sum)) return false;
+
+        seen.add(sum);
         num = sum;
     }
 
