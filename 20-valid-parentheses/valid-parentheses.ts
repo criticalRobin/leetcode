c1a726a566327c67858321fbe72591ee7fbd0e3b
Matias Tite <matiastite1@gmail.com>
2026-09-10 01:58:28 -0500

    Time: 5 ms (42.34%) | Memory: 58.9 MB (16.55%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.ts
@@ -0,0 +1,21 @@
+function isValid(s: string): boolean {
+    if (s.length <= 1) return false;
+
+    const map: Map<string, string> = new Map([
+        [')', '('],
+        ['}', '{'],
+        [']', '['],
+    ]);
+    let openChars: string[] = [];
+
+    for (const char of s) {
+        if (char === '(' || char === '{' || char === '[') {
+            openChars.push(char);
+        } else {
+            if (map.get(char)! !== openChars[openChars.length - 1]) return false;
+            openChars.pop();
+        }
+    }
+
+    return openChars.length > 0 ? false : true;
+};
