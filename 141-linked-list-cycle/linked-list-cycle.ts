029f2b9efd9a87ee87db1279d8c1d2a280ea5ccc
Matias Tite <matiastite1@gmail.com>
2026-09-11 20:07:10 -0500

    Time: 44 ms (80.85%) | Memory: 57.4 MB (85.33%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.ts
+++ 141-linked-list-cycle/linked-list-cycle.ts
@@ -11,19 +11,6 @@
  */
 
 function hasCycle(head: ListNode | null): boolean {
-    if (!head || head.next === null) return false;
-    
-    // const visitedNodes: Set<ListNode> = new Set(); 
-    // let currentNode: ListNode = head;
-    
-    // while (true) {
-    //     if (currentNode.next === null) return false;
-    //     if (visitedNodes.has(currentNode.next)) return true;
-
-    //     visitedNodes.add(currentNode);
-    //     currentNode = currentNode.next;
-    // }
-
     let left: ListNode = head;
     let right: ListNode = head;
 
