aee8b6bcbc5fb59769d2cd00c2d2e0f124a52b88
Matias Tite <matiastite1@gmail.com>
2026-09-15 21:34:55 -0500

    Time: 9 ms (33.47%) | Memory: 59.7 MB (35.57%) - LeetSync

---
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.ts
@@ -0,0 +1,26 @@
+type MathOperation = (a: number, b: number) => number;
+
+function evalRPN(tokens: string[]): number {
+    const operators: Set<string> = new Set(['+', '-', '*', '/']);
+    const stack: string[] = [];
+
+    const calculate: Record<string, MathOperation> = {
+        '+': (a, b) => a + b,
+        '-': (a, b) => b - a,
+        '*': (a, b) => a * b,
+        '/': (a, b) => Math.trunc(b / a),
+    };
+
+    for (const token of tokens) {
+        if (!operators.has(token)) {
+            stack.push(token);
+        } else {
+            let numOne: string = stack.pop()!;
+            let numTwo: string = stack.pop()!;
+            let ans: number = calculate[token](Number(numOne), Number(numTwo));
+            stack.push(String(ans));
+        }
+    }
+    
+    return Number(stack.pop());
+};
