3d7a72919060f4c5521950e0983ad792611880a3
Matias Tite <matiastite1@gmail.com>
2026-09-19 22:14:55 -0500

    Time: 4 ms (17.74%) | Memory: 58.4 MB (16.45%) - LeetSync

---
+++ 67-add-binary/add-binary.ts
@@ -0,0 +1,22 @@
+function addBinary(a: string, b: string): string {
+    let i = a.length - 1;
+    let j = b.length - 1;
+    let carry = 0;
+    let result = '';
+
+    while (i >= 0 || j >= 0 || carry > 0) {
+        const digitA = i >= 0 ? Number(a[i]) : 0;
+        const digitB = j >= 0 ? Number(b[j]) : 0;
+
+        const sum = digitA + digitB + carry;
+        const resultDigit = sum % 2;
+        carry = Math.floor(sum / 2);
+
+        result = String(resultDigit) + result; 
+
+        i--;
+        j--;
+    }
+
+    return result;
+}
