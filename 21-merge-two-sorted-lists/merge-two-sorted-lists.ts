8e17ed4c43c891c98a8de3ee21406977af850230
Matias Tite <matiastite1@gmail.com>
2026-09-23 10:31:25 -0500

    Time: 3 ms (9.53%) | Memory: 58.8 MB (24.79%) - LeetSync

---
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.ts
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
+    const dummyNode: ListNode = new ListNode(0);
+    let current: ListNode = dummyNode;
+
+    while (list1 !== null && list2 !== null) {
+        const val1: number = list1 !== null ? list1.val : 0;
+        const val2: number = list2 !== null ? list2.val : 0;
+
+        if (val1 <= val2) {
+            current.next = new ListNode(val1);
+            if (list1 !== null) list1 = list1.next;
+        } else {
+            current.next = new ListNode(val2);
+            if (list2 !== null) list2 = list2.next;
+        }
+
+        current = current.next;
+    }
+
+    current.next = list1 !== null ? list1 : list2;
+
+    return dummyNode.next;
+};
