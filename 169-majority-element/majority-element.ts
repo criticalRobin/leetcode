50b57be89c5ba05ff0df9ce0ae06535fe1f220f0
Matias Tite <matiastite1@gmail.com>
2026-09-24 23:56:05 -0500

    Time: 26 ms (5.32%) | Memory: 64.7 MB (5.05%) - LeetSync

--- 169-majority-element/majority-element.ts
+++ 169-majority-element/majority-element.ts
@@ -1,26 +1,14 @@
 function majorityElement(nums: number[]): number {
-    let numsMap: Map<number, number> = new Map();
-    let bestNum: number = 0;
-    let bestCount: number = 0;
+    if (nums.length === 1) return nums[0];
 
-    for(let i: number = 0; i < nums.length; i++){
-        if(i === 0){
-            bestNum = nums[i];
-            bestCount = 1;
-        }
-        
-        if(!numsMap.has(nums[i])){
-            numsMap.set(nums[i], 1);
-        }else{
-            let numCount: number = numsMap.get(nums[i]!)
-            numsMap.set(nums[i], ++numCount);
-
-            if(numCount > bestCount){
-                bestNum = nums[i];
-                bestCount = numCount;
-            }
-        }
+    let ans: number = 0;
+    const numsMap: Map<number, number> = new Map();
+    nums.sort((a, b) => a - b);
+    
+    for (const num of nums) {
+        numsMap.set(num, (numsMap.get(num) + 1 | 0));
+        if (numsMap.get(num)! > (numsMap.get(ans) | 0)) ans = num;
     }
 
-    return bestNum;
+    return ans;
 };
