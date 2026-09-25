24346b1ac8dc127aa44988b76dc561540eb9b8e3
Matias Tite <matiastite1@gmail.com>
2026-09-24 23:25:01 -0500

    Time: 66 ms (5.21%) | Memory: 63.7 MB (5.25%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.ts
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.ts
@@ -1,11 +1,13 @@
 function removeDuplicates(nums: number[]): number {
-    let k: number = 1;
+    let k: number = 0; // insert on k + 1
 
-    for (let i: number = 0; i < nums.length; i++) {
-        if (nums[i] !== nums[k - 1]) {
-            nums[k] = nums[i];
+    for (let i: number = 1; i < nums.length; i++) {
+        if (nums[i] !== nums[k]) {
+            console.log(`i: ${nums[i]} - k: ${nums[k]}`);
+            nums[k + 1] = nums[i];
             k++;
         }
     }
-    return k;
+    console.log(`k: ${k}`);
+    return k + 1;
 };
