9d8bfc125f42f8eabdb1db8c888884e277012f5a
Matias Tite <matiastite1@gmail.com>
2026-09-24 23:45:27 -0500

    Time: 52 ms (77.62%) | Memory: 57.9 MB (97.35%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.ts
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.ts
@@ -1,7 +1,7 @@
 function removeDuplicates(nums: number[]): number {
-    let k: number = 1;
+    let k: number = 0;
 
-    for (let i: number = 1; i < nums.length; i++) {
+    for (let i: number = 0; i < nums.length; i++) {
         if (nums[i] !== nums[k - 2]) {
             nums[k] = nums[i];
             k += 1;
